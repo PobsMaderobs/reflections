985d282c5976b97e7ea93fc359bd4c2eb17d5abb
Pablo <pmaderal177@gmail.com>
2015-12-27 22:15:28 -0500

    The coins shall be yellow henceforth

--- game.js
+++ game.js
@@ -725,7 +725,9 @@ Coin = function () {
               0,  5,
               5,  0,
               0, -5]);
-
+  this.color = 'yellow';
+  this.solid = true;
+  
   this.collidesWith = ["ship"];
 
   this.newPosition = function () {
